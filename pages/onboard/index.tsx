b5ee40900417ce10a27283b8c3dc3d7147302dba
PCloud63514 <pcloud63514@gmail.com>
2023-09-22 13:59:17 +0900

    Good

---
+++ pages/onboard/index.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  
+`;
+
+const Button = styled.button``;
+
+function HelloWorld() {
+    return (<>
+        <Container>
+            <Button>하잉</Button>
+        </Container>
+    </>)
+}
+
+export default HelloWorld
